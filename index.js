19c37812d9f27b248da34397dc95915222411195
Olivia Fung New <Olivia@Olivias-MacBook-Pro-7.local>
2020-11-04 16:26:24 -0500

    redefine start variable and focus on returning search results from array of terms in skincare-search-params.csv file. Also added formatting to scraped results

--- index.js
+++ index.js
@@ -1,90 +1,87 @@
+//imports for nightmare.js and d3-dsv for scraping and outputting to csv format
 const { csvFormat } = require('d3-dsv');
 const Nightmare = require('nightmare');
 const { readFileSync, writeFileSync } = require('fs');
-/*const numbers = readFileSync('./tesco-title-numbers.csv', 
-  {encoding: 'utf8'}).trim().split('\n');*/
-const START = 'https://www.dermstore.com/shop_Sale_504310.htm';
+//declare params to input into search bar
+const params = readFileSync('./skincare-search-params.csv', 
+  {encoding: 'utf8'}).trim().split('\n');
+//starting URL for nightmare
+const START = 'https://www.sephora.com/';
 
-  const getSkincare = async () => {
-    //console.log(`Now checking ${id}`);
-    const nightmare = new Nightmare({ show: true, webPreferences: {images: false }});
+//handles some of the string manipulation for the result array,
+function handleResultData(string) {
+  var re = /,/g;
+  var reTwo = /"|'/g;
+  var reThree = /'/g;
+  var reFour = /,/g;
+  replace = string.replace(re,"'\n'").replace(reTwo,'').replace(reThree,'').replace(reFour,"'\n'");
+  var finalResult = replace;
+  return finalResult;
+};
+
+  //declare async function to start scrape
+  const getSkincare = async (param) => {
+    //log statement showing param being passed
+    console.log(`Now checking ${param}`);
+    //({ show: true }) flag brings up a browser window to show work being done by script
+    const nightmare = new Nightmare({ show: true});
     // Go to initial start page, navigate to Detail search
-    /*try {
-        await nightmare
-          .goto(START)
-          .wait('.dropdown-toggle sale-cat')
-          .click('.dropdown-toggle sale-cat');
-      } catch(e) {
-        console.error(e);
-      }*/
     try {
       await nightmare
+        //navigates to URL defined above and finds the search bar,
+        //enters the passed param, waits and presses enter
         .goto(START)
-        .wait('checkbox_500014')
-        .click('checkbox_500014');
+        .wait('.css-1g8idav')
+        .type('.css-1g8idav', param)
+        .wait(500)
+        //simulates enter keystroke to return search results
+        .type('body', '\u000d')
+        //handle popup by waiting and clicking close button
+        .wait('.css-1kna575')
+        .click('.css-1kna575');
     } catch(e) {
       console.error(e);
     }
     try {
-      await nightmare
-        .goto(START)
-        .wait('checkbox_300082')
-        .click('checkbox_300082');
-    } catch(e) {
-      console.error(e);
-    }
-    try {
-      await nightmare
-        .goto(START)
-        .wait('checkbox_500837')
-        .click('checkbox_500837');
-    } catch(e) {
-      console.error(e);
-    }
-    try {
-      await nightmare
-        .goto(START)
-        .wait('checkbox_500001')
-        .click('checkbox_500001');
-    } catch(e) {
-      console.error(e);
-    }
-    try {
-      await nightmare
-        .goto(START)
-        .wait('checkbox_503564')
-        .click('checkbox_503564');
-    } catch(e) {
-      console.error(e);
-    }
-    try {
+      //defines result of search and selects class of items returned,
+      //uses the spread operator to populate an array, and then
+      //iterates through the new array with map and prints innerText
       const result = await nightmare
-        .wait('.item-name')
+        .wait('.css-1gughuu')
         .evaluate(() => {
-          return [...document.querySelectorAll('.item-name')]
+          return [...document.querySelectorAll('.css-1gughuu')]
             .map(el => el.innerText);
         })
+        //ends the process
         .end();
-      return { result: result[0]};
+      //return result array
+      return {result};
     } catch(e) {
       console.error(e);
       return undefined;
     }
   };
 
-
-  getSkincare()
+  //calls function and passes in first index of param file
+  //defined in params and logs result to console. This
+  //call will only work for one param, specified as [0]
+  /*getSkincare(params[0])
   .then(a => console.dir(a))
-  .catch(e => console.error(e));
+  .catch(e => console.error(e));*/
 
 
-  /*const series = numbers.reduce(async (queue, number) => {
+
+  //used to define a series for a list of params and makes async
+  //calls until all params have been passed and the results have
+  //been processed
+
+  const series = params.reduce(async (queue, param) => {
     const dataArray = await queue;
-    dataArray.push(await getAddress(number));
+    dataArray.push(await getSkincare(param));
     return dataArray;
   }, Promise.resolve([]));
   series.then(data => {
     const csvData = csvFormat(data.filter(i => i));
-    writeFileSync('./output.csv', csvData, { encoding: 'utf8' });
+    writeFileSync('./output.csv', handleResultData(csvData), { encoding: 'utf8' });
   })
-  .catch(e => console.error(e));*/
+  .catch(e => console.error(e));
