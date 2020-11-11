9a89fb84421ce1a245cf1f1b41332057ddb87955
Olivia Fung New <Olivia@Olivias-MacBook-Pro-7.local>
2020-11-10 22:05:01 -0500

    add SMTP.py file to generate an email that includes the collected outfrom from twitter_scraper.py

---
+++ SMTP.py
@@ -0,0 +1,101 @@
+import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com" #gmail smtp server address
+sender_email = "ccf2.developer@gmail.com"  # Enter your address
+receiver_email = "ccf2.developer@gmail.com"  # Enter receiver address (oliviakfung@gmail.com)
+password = input("Type your password and press enter: ") #using input to avoid hardcoding password
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "Skincare Public Service Annoucement"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+with open('./output/tweet_text/output.csv') as f:
+    tweet_content = (f.read())
+
+with open('./output/tweet_image/image_url.txt') as f:
+    image_url = (f.read())
+
+message_text = """\
+
+See the LATEST products in skincare!!! Doctors hate these simple tricks
+that will make you look TEN years YOUNGER!!! NUMBER FOUR will SHOCK you!!!
+
+Sean Garrette's Tweets:
+{}
+Sean Garrette's Images Links:
+{}
+""".format(tweet_content, image_url).replace(u"\u2018", "'").replace(u"\u2019", "'").replace(u"\u2026", "'")
+
+image_name = './img/'
+
+message_html = """\
+<html>
+  <body style="background-color:#fbefef;">
+    <img src="cid:image1" alt="silhouette of woman">
+    <section style="font-family:helvetica;">
+    <br>
+    Hey Liv,
+    <br>
+    <br>
+       It is another beautiful day and I am loving the wonky eyelid.
+       Since you are always looking for a reason to shop (probably while
+       I am waiting to watch a show with you) I thought you might want to
+       hear about Sean Garrette's latest picks.
+       <br>
+       <br>
+       <lh> <strong>Useful Links </strong> </lh>
+        <ol>
+            <li><a href="https://www.dermstore.com/">Dermstore</a></li>
+            <li><a href="https://www.sephora.com/">Sephora</a></li>
+            <li><a href="https://www.ulta.com/">Sephora</a></li>
+            <li><a href="https://sokoglam.com/">Soko Glam</a></li>
+        </ol>
+       Below are Sean G's recent twitter mentions, hopefully he has a
+       few suggestions for you.
+    </section>
+    <section style="font-family:helvetica;">
+       <br>
+       <strong> Sean Garrette's Tweets: </strong>
+       <br>
+       {}
+       <br>
+       <br>
+       <strong> Sean Garrette's Images Links: </strong>
+       {}
+    </section>
+  </body>
+</html>
+""".format(tweet_content, image_url)
+
+# This assumes the image is in the current directory
+with open('./image1.png', 'rb') as fp:
+    msgImage = MIMEImage(fp.read(), _subtype="html")
+
+
+# Define the image's ID as referenced above
+msgImage.add_header('Content-ID', '<image1>')
+message.attach(msgImage)
+
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(message_text, "plain")
+part2 = MIMEText(message_html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part1)
+message.attach(part2)
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+        )
+
--- twitter_scraper.py
+++ twitter_scraper.py
@@ -2,6 +2,8 @@
 import tweepy
 import pandas as pd
 import time
+import requests
+import sys
 
 ##### Credentials to access Twitter API
 ##### TODO: Put these in another file and reference them
@@ -18,15 +20,18 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 ##### Twitter user handle to be scraped
 username = 'seangarrette'
 ##### Number of tweets collected
-count = 150
+count = 200
+
+hash_list = []
 
 ##### Function to pick out useful data from tweet scrape
 def handleTweetResults(data):
+      
       ##### Define the new names of your columns
       newcols = {
-      0: 'DateTime', 
-      1: 'Hash', 
-      2: 'Tweet'
+      0: 'DateTime ', 
+      1: 'Hash ', 
+      2: 'Tweet '
       }
       ###### Use `rename()` to rename your columns
       tweets_df.rename(columns=newcols, inplace=True)
@@ -36,20 +41,19 @@ def handleTweetResults(data):
       tweets_df2 = tweets_df[~tweets_df["Tweet"].str.contains("RT")]
       ##### This targets tweets by cutting rows without mention of a sale
       tweets_df3 = tweets_df2[tweets_df2["Tweet"].str.contains("sale", case=False)]
+      print(tweets_df3)
 
       #iterate through rows and print only columns named DateTime and Tweet
-      for index, row in tweets_df2.iterrows():
+      for index, row in tweets_df3.iterrows():
             #####print(row['DateTime'], row['Tweet'])
-            print(row['Hash'])
-      ##### PSEUDO CODE
-      hash_list = printed rows
+            hash_list.append((row['Hash']))
+      print(hash_list)
 
-      #tweets_df3.to_csv (r'./output/output.csv', header=True)
-
+      tweets_df3.to_csv (r'./output/tweet_text/tweet_content.csv', header=True)
 
 try:     
   #Creation of query method using parameters
- tweets = tweepy.Cursor(api.user_timeline,id=username, exclude_replies=True).items(count)
+ tweets = tweepy.Cursor(api.user_timeline,id=username, exclude_replies=True, include_entities=True).items(count)
  
   #Pulling information from tweets iterable object
  tweets_list = [[tweet.created_at, tweet.id, tweet.text]for tweet in tweets]
@@ -64,8 +68,23 @@ except BaseException as e:
 
 handleTweetResults(tweets_df)
 
-##### Pass result of handleTweetResults to status and find image url for saving
 
-#id = handleTweetResults(tweets_df)
-#status = api.get_status(id)
-#print(status.entities)
+##### Pass result of handleTweetResults to status and find image url for saving
+for x in hash_list:
+      status = api.get_status(x)
+      if not status.entities.get('urls'):
+            print('There is no image url in this status')
+      else:
+            url = status.entities.get('urls')[0].get('url')
+            print(url)
+            with open('./output/tweet_image/image_url.txt', 'w+') as f:
+                  print(url, file=f)
+
+###### Alternate method of writing url to file
+#response = requests.get(url)
+            #print(response)
+            #file = open("./output/tweet_images/tweet_image.jpg", "wb")
+            #file.write(response.content)
+            #file.close()
+
+            
